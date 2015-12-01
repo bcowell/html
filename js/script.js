b35cff158c83945aeec91de62172ff0023dd85a5
Brayden Cowell <eacoop2@uoguelph.ca>
2015-12-01 15:38:17 -0500

    Fix for Google Maps API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 44.536479, lng: -79.7330677},
     zoom: 8
   });
+}
